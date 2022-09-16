50691b878705a271ee807bba3c0d8dceb687b50b
unknown <lautarorangilt@gmail.com>
2022-09-16 14:22:14 -0300

    marketplace

--- bearbuilders-frontend/pages/index.js
+++ bearbuilders-frontend/pages/index.js
@@ -1,8 +1,33 @@
+import React from 'react'
+import axios from 'axios'
+import Link from 'next/link';
 
-export default function Home() {
+
+export default function index({ reservations }) {
   return (
-    <div >
-      <h1>Manijas dev</h1>
+    <div>
+      {reservations.map(reservation => (
+        <Link href={`/reservations/${reservation.id}`} key={reservation.id}>
+          <a>
+            <div>
+              <h1>hotel: {reservation.hotel.name}</h1>
+            </div>
+          </a>
+        </Link>
+      ))}
     </div>
   )
+}
+
+
+export const getServerSideProps = async (context) => {
+
+  const { data: reservations } = await axios.get('http://localhost:3002/reservations')
+  // console.log(res.data)
+
+  return {
+    props: {
+      reservations
+    }
+  }
 }
