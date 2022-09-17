4c741edd73184f9d9cbe8d61bf21766beb3ceb5f
Leo Romero <leonardo.romero@mercadolibre.com>
2022-09-17 16:56:25 -0300

    styles

--- bearbuilders-frontend/components/Navigation/Navigation.js
+++ bearbuilders-frontend/components/Navigation/Navigation.js
@@ -1,9 +1,10 @@
-import { Navbar, Button, Link, Text } from "@nextui-org/react";
+import { Navbar, Button, Text } from "@nextui-org/react";
 import Layout from "./Layout.js";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import Link from 'next/link'
 
 export default function Navigation() {
   const { status } = useAccount();
@@ -33,22 +34,23 @@ export default function Navigation() {
       <Navbar variant="sticky">
         <Navbar.Brand>
           <Link href="/">
-            <Image src="/img/hotel-bell.png" width={30} height={25} />
-            <Text
-              weight="extrabold"
-              color="#8a6445"
-              hideIn="xs"
-              css={{ marginLeft: "1rem" }}
-              size={20}
-            >
-              TICKO
-            </Text>
+            <a className='navbar-logo'>
+              <Image src="/img/hotel-bell.png" width={30} height={25} />
+              <Text
+                weight="extrabold"
+                color="#8a6445"
+                hideIn="xs"
+                css={{ marginLeft: "1rem" }}
+                size={20}
+              >
+                TICKO
+              </Text>
+            </a>
           </Link>
         </Navbar.Brand>
         <Navbar.Content enableCursorHighlight hideIn="xs" variant="underline">
-          <Navbar.Link href="/">Inicio</Navbar.Link>
-          <Navbar.Link href="/marketplace">Marketplace</Navbar.Link>
-          <Navbar.Link href="/">Nosotros</Navbar.Link>
+          <Link href="/"><a >Inicio</a></Link>
+          <Link href="/marketplace"><a >Marketplace</a></Link>
         </Navbar.Content>
         <Navbar.Content>
           <Navbar.Item>
