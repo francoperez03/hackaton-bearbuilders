d3128842016f80548c5cf66c7ab8bfd2783d91a3
Franco Perez <franco.perez03@gmail.com>
2022-09-17 15:48:21 -0300

    fix hydration

--- bearbuilders-frontend/components/Navigation/Navigation.js
+++ bearbuilders-frontend/components/Navigation/Navigation.js
@@ -1,61 +1,72 @@
-import { Navbar, Button, Link, Text, useTheme } from "@nextui-org/react";
+import { Navbar, Button, Link, Text } from "@nextui-org/react";
 import Layout from "./Layout.js";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
-import Image from 'next/image'
+import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function Navigation() {
-
-   const { address, status } = useAccount();
-   const getButtonProfile = () => {
-      if (status) {
-         if (status === "connected") {
-            return (
-               <Button auto flat as={Link} href="/profile">
-                  Mi Perfil
-               </Button>
-            );
-         } else {
-            return (
-               <ConnectButton
-                  label="Conectá tu billetera"
-                  accountStatus="address"
-                  chainStatus="none"
-                  showBalance={false}
-               />
-            );
-         }
+  const { status } = useAccount();
+  const [button, setButton] = useState(<></>);
+  console.log({ status });
+  useEffect(() => {
+    if (status) {
+      if (status === "connected") {
+        setButton(
+          <Button auto flat as={Link} href="/profile">
+            Mi Perfil
+          </Button>
+        );
       } else {
-         return <></>;
+        setButton(
+          <ConnectButton
+            label="Conectá tu billetera"
+            accountStatus="address"
+            chainStatus="none"
+            showBalance={false}
+          />
+        );
       }
-   };
+    } else {
+      setButton(<></>);
+    }
+  }, [status]);
 
-   return (
-      <Layout>
-         <Navbar variant="sticky" >
-            <Navbar.Brand>
-               <Link href="/">
-                  <a>
-                     <Image src='/img/hotel-bell.png' width={30} height={25} />
-                     <Text weight="extrabold" color="#8a6445" hideIn="xs" css={{ marginLeft: "1rem" }} size={20}>
-                        TICKO
-                     </Text>
-                  </a>
-               </Link>
-            </Navbar.Brand>
-            <Navbar.Content enableCursorHighlight hideIn="xs" variant="underline">
-               <Navbar.Link href="/">Inicio</Navbar.Link>
-               <Navbar.Link href="/marketplace">
-                  Marketplace
-               </Navbar.Link>
-               <Navbar.Link href="/">Nosotros</Navbar.Link>
-            </Navbar.Content>
-            <Navbar.Content>
-               <Navbar.Item>
-                  {getButtonProfile()}
-               </Navbar.Item>
-            </Navbar.Content>
-         </Navbar>
-      </Layout>
-   )
+  // switch (status) {
+  //    case "connected": return<></>;
+  //    case "": return<></>;
+  //    default : return<></>;
+  // }
+  return (
+    <Layout>
+      <Navbar variant="sticky">
+        <Navbar.Brand>
+          <Link href="/">
+            <a>
+              <Image src="/img/hotel-bell.png" width={30} height={25} />
+              <Text
+                weight="extrabold"
+                color="#8a6445"
+                hideIn="xs"
+                css={{ marginLeft: "1rem" }}
+                size={20}
+              >
+                TICKO
+              </Text>
+            </a>
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Content enableCursorHighlight hideIn="xs" variant="underline">
+          <Navbar.Link href="/">Inicio</Navbar.Link>
+          <Navbar.Link href="/marketplace">Marketplace</Navbar.Link>
+          <Navbar.Link href="/">Nosotros</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Item>
+            <>{button}</>
+          </Navbar.Item>
+        </Navbar.Content>
+      </Navbar>
+    </Layout>
+  );
 }
